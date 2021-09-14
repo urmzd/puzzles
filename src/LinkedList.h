485394387e6a4f3de03e810338942e992d21efba
Urmzd <urmzds@gmail.com>
2021-09-13 22:04:29 -0400

    Make node generic and remove list wrapper

--- src/LinkedList.h
+++ src/LinkedList.h
@@ -1,16 +1,11 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
-typedef struct OneWayNode {
-  int data;
-  struct OneWayNode *next;
-} one_way_node;
+typedef struct Node {
+  void *data;
+  struct Node *next;
+} node;
 
-typedef struct LinkedList {
-  one_way_node *head;
-} linked_list;
-
-extern linked_list *new_linked_list();
-extern one_way_node *new_way_node(int data);
+extern node *node_new(void *data);
 
 #endif
