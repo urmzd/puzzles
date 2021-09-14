396ae0d544c279c9df4bdd0b626b4770271c30af
Urmzd <urmzds@gmail.com>
2021-09-13 22:26:39 -0400

    Pass function references

---
+++ src/LinkedList.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "LinkedList.h"
+
+extern int node_free(node *node) { return 0; }
+
+extern node *node_insert(node *head, void *data, node_new_fn *node_new_fn) {
+
+  return NULL;
+}
+
+extern node *node_new(void *data) {
+  node *new_node = malloc(sizeof(node));
+  new_node->data = data;
+  return new_node;
+}
--- src/LinkedList.h
+++ src/LinkedList.h
@@ -6,8 +6,12 @@ typedef struct Node {
   struct Node *next;
 } node;
 
+typedef node *(*node_new_fn)(void *data);
+
 extern node *node_new(void *data);
-extern node *node_insert(node *head, void *data);
-extern node *node_free(node *node);
+
+extern node *node_insert(node *head, void *data, node_new_fn *node_new_fn);
+
+extern int node_free(node *node);
 
 #endif
